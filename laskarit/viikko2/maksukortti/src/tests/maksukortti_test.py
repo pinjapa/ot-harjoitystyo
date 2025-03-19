09a8ea0254a7ebd56708846581eb8fee236987a7
Pinja Paivakumpu <paivakumpupinja@gmail.com>
2025-03-19 17:48:47 +0200

    task 3

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -24,13 +24,36 @@ class TestMaksukortti(unittest.TestCase):
         kortti.syo_edullisesti()
 
         self.assertEqual(kortti.saldo_euroina(), 2.0)
+
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self): #task 3
+        kortti = Maksukortti(200)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
     
     def test_kortille_voi_ladata_rahaa(self):
         self.kortti.lataa_rahaa(2500)
 
         self.assertEqual(self.kortti.saldo_euroina(), 35.0)
+    
+    def test_negatiivisen_summan_lataaminen_ei_muuta_saldoa(self): #task 3
+        self.kortti.lataa_rahaa(-200)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 10.0)
 
     def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
         self.kortti.lataa_rahaa(20000)
 
         self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+    
+    def test_voi_ostaa_edullisen_lounaan_kun_lounaan_verran_kortilla(self): #task 3
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0)
+
+    def test_voi_ostaa_maukkaan_lounaan_kun_lounaan_verran_kortilla(self): #task 3
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0)
